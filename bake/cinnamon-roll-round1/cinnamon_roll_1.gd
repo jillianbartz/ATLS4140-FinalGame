6a5e0db4cd59896f1205275bfbbeaff01a8297fc
Jillian Bartz <bartzjillian@gmail.com>
2024-11-13 14:10:25 -0700

    added chef, butter, flour, and egg particles

--- bake/cinnamon-roll-round1/cinnamon_roll_1.gd
+++ bake/cinnamon-roll-round1/cinnamon_roll_1.gd
@@ -12,6 +12,7 @@ var spawned: bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	$Chef/ChefSprite/AnimationPlayer.play("Idle")
 	state = States.EGG
 
 func spawn_eggs():

dacdd790a4c6997cdafe666a93c54b9812e4f11a
Jillian Bartz <bartzjillian@gmail.com>
2024-12-12 13:58:04 -0700

    finished butter edits

--- bake/cinnamon-roll-round1/butter.gd
+++ bake/cinnamon-roll-round1/butter.gd
@@ -7,43 +7,57 @@ var up3: bool = true
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$Timer.start(.05)
-	$Area2D3.rotation = deg_to_rad(-12)
+	$BigKnife1.rotation = deg_to_rad(-12)
 
 func _process(delta: float) -> void:
-	$Area2D.global_position.x -= .8
-	$Area2D2.global_position.x -= .8
-	$Area2D3.global_position.x -= .8
+	$BigKnife1.global_position.x -= .8
+	$BigKnife2.global_position.x -= .8
+	$MedKnife1.global_position.x -= .8
+	$MedKnife2.global_position.x -= .8
+	$SmallKnife1.global_position.x -= .8
+	$SmallKnife2.global_position.x -= .8
 	$Butter.global_position.x -= .8
 
 func area1_rotation():
 	if(up1):
-		if($Area2D.rotation >= deg_to_rad(15)):
+		if($BigKnife1.rotation >= deg_to_rad(15)):
 			up1 = false
-		$Area2D.rotation += deg_to_rad(1)
+		$BigKnife1.rotation += deg_to_rad(1)
+		$BigKnife2.rotation += deg_to_rad(1)
+		
 	else:
-		if($Area2D.rotation <= deg_to_rad(-15)):
+		if($BigKnife1.rotation <= deg_to_rad(-15)):
 			up1 = true
-		$Area2D.rotation -= deg_to_rad(1)
+		$BigKnife1.rotation -= deg_to_rad(1)
+		$BigKnife2.rotation -= deg_to_rad(1)
 
 func area2_rotation():
 	if(up2):
-		if($Area2D2.rotation >= deg_to_rad(15)):
+		if($MedKnife1.rotation >= deg_to_rad(10)):
 			up2 = false
-		$Area2D2.rotation += deg_to_rad(2.5)
+		$MedKnife1.rotation += deg_to_rad(1.3)
+		$MedKnife2.rotation += deg_to_rad(1.3)
+		
 	else:
-		if($Area2D2.rotation <= deg_to_rad(-15)):
+		if($MedKnife1.rotation <= deg_to_rad(-10)):
 			up2 = true
-		$Area2D2.rotation -= deg_to_rad(2.5)
+		$MedKnife1.rotation -= deg_to_rad(1.3)
+		$MedKnife2.rotation -= deg_to_rad(1.3)
+		
 
 func area3_rotation():
 	if(up3):
-		if($Area2D3.rotation >= deg_to_rad(10)):
+		if($SmallKnife1.rotation >= deg_to_rad(12)):
 			up3 = false
-		$Area2D3.rotation += deg_to_rad(1.3)
+		$SmallKnife1.rotation += deg_to_rad(2)
+		$SmallKnife2.rotation += deg_to_rad(2)
+		
 	else:
-		if($Area2D3.rotation <= deg_to_rad(-10)):
+		if($SmallKnife1.rotation <= deg_to_rad(-12)):
 			up3 = true
-		$Area2D3.rotation -= deg_to_rad(1.3)
+		$SmallKnife1.rotation -= deg_to_rad(2)
+		$SmallKnife2.rotation -= deg_to_rad(2)
+		
 
 func _on_timer_timeout() -> void:
 	area1_rotation()
